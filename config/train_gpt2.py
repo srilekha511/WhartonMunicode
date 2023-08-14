da030ea5c07f871752ef1a8b697a28012931b207
srilekha511 <53538430+srilekha511@users.noreply.github.com>
2023-08-14 14:27:32 -0400

    script to train GPT2

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -1,4 +1,4 @@
-# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# config for training GPT-2 (124M)
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
