f154d34145c1d5be9246ec8ba16de9453b6c67ed
srilekha511 <53538430+srilekha511@users.noreply.github.com>
2023-08-18 16:33:45 -0400

    Update prepare.py

--- data/municode/prepare.py
+++ data/municode/prepare.py
@@ -6,7 +6,7 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    data_url = 'https://github.com/srilekha511/municode_files/blob/main/AlvaradoTXComplete.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
